9c45fa52fd60252e232b373bcf3c69612818ae6b
R3D-ubuntu <ryuk24.gaming@gmail.com>
2026-08-04 13:54:29 +0200

    stdiopp.h

---
+++ stdiopp.h
@@ -0,0 +1,15 @@
+#ifndef STDIOPP_H
+#define STDIOPP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void set_stdout_buf_size(unsigned long size) {
+  if (setvbuf(stdout, malloc(size), _IOFBF, size) != 0)
+    return 1;
+  return 0;
+}
+
+#endif // STDIOPP_H
+
